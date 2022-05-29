17ae7ad51fdcc82511c560d454d3faf9f6a4cfc7
Scott Sullivan <scott@sullysbrain.com>
2022-05-29 16:46:01 -0400

    Additional scripts created for University of Michigan Python course.

--- 2_data_structures/strings.py
+++ 2_data_structures/strings.py
@@ -1,4 +1,14 @@
-text = "X-DSPAM-Confidence:    0.8475"
-slicePos = text.find('0')
-sliced = float(text[slicePos:])
-print(sliced)
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+total = 0.0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    count = count + 1
+    nmbPos = line.find(': ')
+    nmbStr = line[nmbPos+2:].rstrip()
+    total = total + float(nmbStr)
+    avg = float(total) / count
+print("Average spam confidence:", avg)
